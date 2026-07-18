d410d0835e3f67cde1af7f0998639ea395fc1601
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2026-07-19 03:14:39 +0500

    Made some changes

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "./generated/prisma/client.js";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
@@ -31,4 +31,4 @@ function createClient() {
 export const prisma = globalForPrisma.prisma ?? createClient();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export * from "./generated/prisma/client";
+export * from "./generated/prisma/client.js";
